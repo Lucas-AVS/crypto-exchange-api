3aae3b89fc7d24f2d54d4ef7e42b84e138a9365b
Lucas-AVS <lucas.alencarvs@gmail.com>
2025-09-12 12:51:28 -0300

    refactor: rename UsersRepo → UserJpaRepository, move to repository/jpa, implement UserRepository and add @Profile("jpa")

--- src/main/java/com/lucasavs/cryptoexchange/repo/UsersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.lucasavs.cryptoexchange.repo;
-
-import com.lucasavs.cryptoexchange.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UsersRepo extends JpaRepository<User, UUID> {
-}
---
+++ src/main/java/com/lucasavs/cryptoexchange/repository/jpa/UserJpaRepository.java
@@ -0,0 +1,16 @@
+package com.lucasavs.cryptoexchange.repository.jpa;
+
+import com.lucasavs.cryptoexchange.entity.User;
+import com.lucasavs.cryptoexchange.repository.UserRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+@Profile("jpa")
+public interface UserJpaRepository
+        extends JpaRepository<User, UUID>, UserRepository {
+    // custom query ex: Optional<User> findByEmail(String email);
+}
