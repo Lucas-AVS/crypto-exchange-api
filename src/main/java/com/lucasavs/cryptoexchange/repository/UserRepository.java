e6891f0da732ddee576ae2b8174c47930ba907bf
Lucas-AVS <lucas.alencarvs@gmail.com>
2025-09-12 20:20:45 -0300

    refactor: update UserRepository to use generic save signature for compatibility with JpaRepository

--- src/main/java/com/lucasavs/cryptoexchange/repository/UserRepository.java
+++ src/main/java/com/lucasavs/cryptoexchange/repository/UserRepository.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository {
-    User save(User user);
+    <S extends User> S save(S user);
+
     Optional<User> findById(UUID id);
+
     List<User> findAll();
 }
