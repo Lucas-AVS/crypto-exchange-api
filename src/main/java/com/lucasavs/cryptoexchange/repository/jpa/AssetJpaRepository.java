8788c6b1339145eed98e3c374d3e873cdfa5bd54
Lucas-AVS <lucas.alencarvs@gmail.com>
2025-09-20 20:56:44 -0300

    feat: new entity and repository for Asset

---
+++ src/main/java/com/lucasavs/cryptoexchange/repository/jpa/AssetJpaRepository.java
@@ -0,0 +1,15 @@
+package com.lucasavs.cryptoexchange.repository.jpa;
+
+import com.lucasavs.cryptoexchange.entity.Asset;
+import com.lucasavs.cryptoexchange.repository.AssetRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+@Profile("jpa")
+public interface AssetJpaRepository extends JpaRepository<Asset, String>, AssetRepository {
+    // custom query ex: Optional<User> findByEmail(String email);
+}
