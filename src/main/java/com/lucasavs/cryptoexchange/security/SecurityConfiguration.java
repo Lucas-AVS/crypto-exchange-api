09bbc9039128460a6b9a015536a46c84703ab744
Lucas-AVS <lucas.alencarvs@gmail.com>
2025-10-07 11:16:37 -0300

    chore: correct asset endpoint "api/assets" + config asset to public route in security

--- src/main/java/com/lucasavs/cryptoexchange/security/SecurityConfiguration.java
+++ src/main/java/com/lucasavs/cryptoexchange/security/SecurityConfiguration.java
@@ -29,7 +29,8 @@ public class SecurityConfiguration {
     private static final String[] AUTH_WHITE_LIST = {
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            "/swagger-ui.html"
+            "/swagger-ui.html",
+            "/api/assets/**"
     };
     @Autowired
     private SecurityFilter securityFilter;
