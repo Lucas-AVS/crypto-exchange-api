dd7f018379a260a2a63f46900e2c9f6229f54d83
Lucas-AVS <lucas.alencar31@gmail.com>
2025-10-22 19:35:38 -0300

    refactor: Delegate primary auth validation to API Gateway, now just handle token generation and internal auth via trusted header

--- src/main/java/com/lucasavs/cryptoexchange/security/TokenService.java
+++ src/main/java/com/lucasavs/cryptoexchange/security/TokenService.java
@@ -3,7 +3,6 @@ package com.lucasavs.cryptoexchange.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.lucasavs.cryptoexchange.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,19 +26,6 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token", exception);
-        }
-    }
-
-    public String validateToken(String token){
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
-                    .withIssuer("auth-api")
-                    .build()
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException exception){
-            return "";
         }
     }
 
