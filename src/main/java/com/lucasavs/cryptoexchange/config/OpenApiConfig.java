fe96267de4ac2c29d52c0a4f00403efcbefde3fc
Lucas-AVS <lucas.alencar31@gmail.com>
2025-10-22 19:38:21 -0300

    chore: configure OpenAPI security and gateway port

---
+++ src/main/java/com/lucasavs/cryptoexchange/config/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.lucasavs.cryptoexchange.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server gatewayServer = new Server()
+                .url("http://localhost:9000") // api gateway URL
+                .description("API Gateway");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth", securityScheme);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+        return new OpenAPI()
+                .servers(List.of(gatewayServer))
+                .components(components)
+                .addSecurityItem(securityRequirement);
+    }
+}
